9881c253fdc45ba707c69a70571f47535f2d2535
Danilo Carvalho <xdanrj@gmail.com>
2024-06-04 08:26:56 -0300

    8h30

--- src/infra/db.js
+++ src/infra/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 // Connection with mongoDB
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.CONNECTION_DB);
     console.log("Successful connection with the database!");
   } catch (error) {
     console.error("Failed to connect with the database:", error.message);
