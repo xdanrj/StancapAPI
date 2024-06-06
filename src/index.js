9ea12f5ded3c0970f30405962a8f4a9ab2c6eba9
xdanrj <xdanrj@gmail.com>
2024-06-06 11:48:58 -0300

    11h50

--- src/index.js
+++ src/index.js
@@ -13,13 +13,6 @@ app.use(express.json());
 
 mongoose.set("strictQuery", false);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(
   // Added to capture user email
   express.urlencoded({
